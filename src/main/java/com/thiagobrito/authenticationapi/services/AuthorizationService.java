26c8dff232d0a8ce3485afa3ff5a61dc0479f1d7
Thiago Brito <thiagobrito74@gmail.com>
2023-11-26 16:49:31 -0400

    feat: create UserRepository and AuthorizationService

---
+++ src/main/java/com/thiagobrito/authenticationapi/services/AuthorizationService.java
@@ -0,0 +1,23 @@
+package com.thiagobrito.authenticationapi.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.thiagobrito.authenticationapi.repositories.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private UserRepository repository;
+
+    public AuthorizationService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
